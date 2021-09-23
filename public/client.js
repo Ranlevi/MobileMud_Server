343c51467f970b89d645dbacd9b4b2dd34541f85
RanLevi <ran.levi@gmail.com>
2021-09-23 19:06:43 +0300

    added react to client

--- public/client.js
+++ public/client.js
@@ -1,3 +1,24 @@
+import * as React from 'react';
+import { TextField } from '@mui/material';
+
+function App() {
+
+    function process_input(text){
+        console.log(text);
+    }
+
+    return (
+        <div>
+    <TextField fullWidth onChange={(event)=> process_input(event.target.value)}/>
+    </div>
+    )
+  }
+  
+ReactDOM.render(<App />, document.querySelector('#app'));
+
+
+
+
 let ws = new WebSocket('ws://localhost:8080');
 let el;
 
