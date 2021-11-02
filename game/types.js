1447e2c3070ee1c5ed066b4a8462434d962d2411
RanLevi <ran.levi@gmail.com>
2021-11-02 18:11:16 +0200

    more rooms

--- game/types.js
+++ game/types.js
@@ -25,27 +25,21 @@ const Types = {
       initialState: "Default",
       Default: {
         actions: {
-          onEnter(params_obj) {
-            console.log("Default: onEnter");
-          },
-          onExit(params_obj){
-            console.log("Default: onExit");
-          }
+          onEnter(params_obj) {},
+          onExit(params_obj) {},         
         },
         transitions: {
           user_enters_room: {
             target: "Greeting",
-            action(params_obj){
-              console.log("transition action from default to greeting");
-            }
+            action(params_obj){},            
           }
         }
       },
       Greeting: {
         actions: {
           onEnter(params_obj) {
-            let owner= World.world.get_instance(params_obj.owner_id);
-            let entity = World.world.get_instance(params_obj.sender_id);
+            let owner=  World.world.get_instance(params_obj.owner_id);
+            let entity= World.world.get_instance(params_obj.sender_id);
 
             if (owner.props["state_variables"][entity.id]!==undefined && 
                 owner.props["state_variables"][entity.id].has_entered_room){
@@ -59,16 +53,12 @@ const Types = {
             }
           
           },
-          onExit(params_obj){
-            console.log("Greeting: onExit");
-          }
+          onExit(params_obj){}
         },
         transitions: {
           tick: {
             target: "Default",
-            action(params_obj){
-              console.log("transition action from greeting to default");
-            }
+            action(params_obj){}
           }
         }
       }
